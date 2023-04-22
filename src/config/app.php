e4d63b58f3fd88d4492e4742346d1ead78a13395
root <root@DESKTOP-PJBDVT1.localdomain>
2023-04-23 07:07:00 +0900

    20230423

--- src/config/app.php
+++ src/config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     |
     */
-
+    // envというヘルパ関数を使用している。.envの中のAPP_NAMEを呼び出している。指定がなければデフォルトでLaravelを返す
     'name' => env('APP_NAME', 'Laravel'),
 
     /*
--- src/routes/web.php
+++ src/routes/web.php
@@ -16,3 +16,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', function () {
+    $posts=[
+        'Title A',
+        'Title B',
+        'Title C',
+    ];
+    
+    return view('index')
+        ->with(['posts' => $posts]);
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
